9da0657e24cfb96cc93f38de08b00282a0c5504d
R. C. Jones <rcjones@uk.ibm.com>
2015-09-23 08:34:09 +0100

    Fixed code style errors per jscs reports

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-/*
-Copyright 2015 IBM Corp. All Rights Reserved.
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 var request = require('request');
 var async = require('async');
@@ -21,7 +21,7 @@ var Service = require('./service.js');
 
 var defaultOptions = {
   strictSSL: true,
-}
+};
 
 module.exports = function(options) {
   if (options.uri == null) {
@@ -47,15 +47,15 @@ module.exports = function(options) {
           asyncTasks.push(function(asyncCallback) {
             services.push(service.ServiceName);
             asyncCallback();
-          })
-        })
+          });
+        });
 
         async.parallel(asyncTasks, function() {
           callback(null, services);
-        })
+        });
       }
-    })
-  }
+    });
+  };
 
   this.getService = function(serviceName, callback) {
     var options = {};
@@ -70,6 +70,6 @@ module.exports = function(options) {
         var serviceData = JSON.parse(body);
         callback(null, new Service(options, serviceName, serviceData.zosConnect.serviceInvokeURL));
       }
-    })
-  }
-}
+    });
+  };
+};
--- service.js
+++ service.js
@@ -1,18 +1,18 @@
-/*
-Copyright 2015 IBM Corp. All Rights Reserved.
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 var request = require('request');
 var extend = require('extend');
@@ -29,8 +29,8 @@ module.exports = function(options, serviceName, invokeUri) {
     options.uri = this.invokeUri;
     options.json = true;
     options.body = data;
-    request(options, callback)
-  }
+    request(options, callback);
+  };
 
   this.getRequestSchema = function(callback) {
     var options = {};
@@ -38,14 +38,14 @@ module.exports = function(options, serviceName, invokeUri) {
     options.uri += '?action=getRequestSchema';
     request.get(options, function(error, response, body) {
       if (error) {
-        callback(error, null)
+        callback(error, null);
       } else if (response.statusCode != 200) {
         callback(new Error('Failed to get schema (' + response.statusCode + ')'), null);
       } else {
         callback(null, body);
       }
-    })
-  }
+    });
+  };
 
   this.getResponseSchema = function(callback) {
       var options = {};
@@ -59,6 +59,6 @@ module.exports = function(options, serviceName, invokeUri) {
         } else {
           callback(null, body);
         }
-      })
-    }
-}
+      });
+    };
+};
